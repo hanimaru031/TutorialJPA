208c7b8cabfb412fee493b3357a6a05bb51caf63
hanimaru031 <haruri0231@gmail.com>
2023-12-27 22:42:27 +0900

    TutorialJPA

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -26,46 +26,47 @@ public class CountryController {
         return "country/list";
     }
 
-     // ----- 追加:ここから -----
-        // ----- 詳細画面 -----
-        @GetMapping(value = { "/detail", "/detail/{code}/" })
-        public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
-            // codeが指定されていたら検索結果、無ければ空のクラスを設定
-            Country country = code != null ? service.getCountry(code) : new Country();
-            // Modelに登録
-            model.addAttribute("country", country);
-            // country/detail.htmlに画面遷移
-            return "country/detail";
-        }
-
-        // ----- 更新（追加） -----
-        @PostMapping("/detail")
-        public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
-                @RequestParam("population") int population, Model model) {
-            // 更新（追加）
-            service.updateCountry(code, name, population);
-
-            // 一覧画面にリダイレクト
-            return "redirect:/country/list";
-        }
-
-        // ----- 削除画面 -----
-        @GetMapping("/delete")
-        public String deleteCountryForm(Model model) {
-            // country/delete.htmlに画面遷移
-            return "country/delete";
-        }
-
-        // ----- 削除 -----
-        @PostMapping("/delete")
-        public String deleteCountry(@RequestParam("code") String code, Model model) {
-            // 削除
-            service.deleteCountry(code);
-
-            // 一覧画面にリダイレクト
-            return "redirect:/country/list";
-        }
-        // ----- 追加:ここまで -----
+    // ----- 追加:ここから -----
+    // ----- 詳細画面 -----
+    @GetMapping(value = { "/detail", "/detail/{code}/" })
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のクラスを設定
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country);
+        // country/detail.htmlに画面遷移
+        return "country/detail";
     }
 
+    // ----- 更新（追加） -----
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name,
+            @RequestParam("population") int population, Model model) {
+        // 更新（追加）
+        service.updateCountry(code, name, population);
 
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+
+    // ----- 削除画面 -----
+    @GetMapping(value = { "/delete", "/delete/{code}/" })
+    public String getCountry1(@PathVariable(name = "code", required = false) String code, Model model) {
+        // country/delete.htmlに画面遷移
+
+        return "country/delete";
+    }
+
+    // ----- 削除 -----
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code") String code, Model model) {
+        // 削除
+
+        service.deleteCountry(code);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+    // ----- 追加:ここまで -----
+
+}
